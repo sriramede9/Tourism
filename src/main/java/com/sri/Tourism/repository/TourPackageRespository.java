4c8ad892ce1cdfde98d0fc2683cf93028766a24d
sriramede9 <only4programming03@gmail.com>
2020-07-02 16:44:56 +0100

    http://localhost:8090/tourPackages/search/findByName?name=Backpack%20Cal

--- src/main/java/com/sri/Tourism/repository/TourPackageRespository.java
+++ src/main/java/com/sri/Tourism/repository/TourPackageRespository.java
@@ -2,9 +2,10 @@ package com.sri.Tourism.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.sri.Tourism.model.TourPackage;
 
 public interface TourPackageRespository extends CrudRepository<TourPackage, String> {
-	TourPackage findByName(String name);
+	TourPackage findByName(@Param("name") String name);
 }
--- src/main/java/com/sri/Tourism/repository/TourRepository.java
+++ src/main/java/com/sri/Tourism/repository/TourRepository.java
@@ -3,10 +3,11 @@ package com.sri.Tourism.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.sri.Tourism.model.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
-	List<Tour> findByTourPackageCode(String code);
+	List<Tour> findByTourPackageCode(@Param("code") String code);
 
 }
