5c551b9c12bc0099a83d0cf7f2a2216d1316ae0d
sriramede9 <only4programming03@gmail.com>
2020-07-02 17:13:14 +0100

    added REstrictions to delete Method

--- src/main/java/com/sri/Tourism/repository/TourRepository.java
+++ src/main/java/com/sri/Tourism/repository/TourRepository.java
@@ -4,10 +4,41 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.sri.Tourism.model.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
 	List<Tour> findByTourPackageCode(@Param("code") String code);
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void delete(Tour entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	@RestResource(exported = false)
+
+	default void deleteAll(Iterable<? extends Tour> entities) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	@RestResource(exported = false)
+
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
